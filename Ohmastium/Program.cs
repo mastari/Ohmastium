3276c6d007ce735be5b43145210723fd9ec478a9
oriont <elijahotarr@gmail.com>
2018-12-01 17:32:01 -0800

    adding cs code

---
+++ Ohmastium/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ohmastium {
+    public static class Program {
+        [STAThread]
+        static void Main() {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
